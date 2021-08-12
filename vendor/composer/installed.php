2c3b48ddf50913053c0f4fe80462cf30c7838526
DiegoElgart <diegojelgart@gmail.com>
2021-08-12 12:00:17 +0300

    started composer project, installed twilio\sdk

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '39fc60356918b063e454b69f5718c95c8beeebc0',
+        'name' => 'diego/twilio-whatsapp-app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/twilio-whatsapp-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '39fc60356918b063e454b69f5718c95c8beeebc0',
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '6.28.0',
+            'version' => '6.28.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'reference' => 'bf3fc7e3a4619923bbc7d57817a52b8f016da951',
+            'dev_requirement' => false,
+        ),
+    ),
+);
