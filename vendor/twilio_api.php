922eecd82a7b48d5e75e39820f88bc2cdd932e2b
DiegoElgart <diegojelgart@gmail.com>
2021-08-12 12:17:39 +0300

    created twilio_app.php for sending the messages and created services folder and getNumberAndLink.php in order to get from the front end the data

---
+++ vendor/twilio_api.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "../vendor/autoload.php";
+
+use Twilio\Rest\Client;
+
+$sid = getenv("TWILIO_ACCOUNT_SID");
+$token = getenv("TWILIO_AUTH_TOKEN");
+
+$_phoneNumber= array();
+$_link= array();
+
+for($i=0; $i<=count($_phoneNumber);$i++){
+
+
+    
+}
+
+
+?>
