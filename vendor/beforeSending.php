2a2e19eafb6532fb58efc8c428264a21e9d6a591
DiegoElgart <diegojelgart@gmail.com>
2021-09-26 15:36:03 +0300

    simplified the app to work on single page

--- vendor/beforeSending.php
+++ vendor/beforeSending.php
@@ -1,3 +1,31 @@
+<?php
+
+//Ternary is for handling error of empty array
+$numbers1=explode("\r\n", $_POST["numbers"]?? "");
+$links=explode("\r\n",$_POST["links"]??"");
+$message=array($_POST["message"]?? "");
+$numbers=preg_replace("/05/","+9725", $numbers1, 1);
+
+$counter= count($numbers);
+//echo $counter;
+//print_r ($numbers) ;
+//print_r ($links);
+//print_r($message);
+
+if($numbers[0] !='' && $links[0] !='' && $message[0] !=''){
+ } else {
+   
+    echo "Please fill the empty boxes" ;
+  };
+
+/*print_r ($numbers);
+echo '<br>';
+echo '<br>';
+print_r($links);
+*/
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,35 +37,9 @@
 <body>
     <h1>TEST</h1>
 <form action="sendMessage.php">
-  <h2>Are you sure you want to send whatsapp to <? echo $counter; ?> persons?</h2>
+  <h2>Are you sure you want to send whatsapp to <?php echo $counter; ?> persons?</h2>
   <input type="submit" value="Yes">
 </form>
 </body>
 </html>
 
-<?php
-
-
-//Ternary is for handling error of empty array
-$numbers1=explode("\r\n", $_POST["numbers"]?? "");
-$links=explode("\r\n",$_POST["links"]??"");
-$message=array($_POST["message"]?? "");
-$numbers=preg_replace("/05/","+9725", $numbers1, 1);
-
-$counter= count($numbers);
-echo $counter;
-//print_r ($numbers) ;
-//print_r ($links);
-//print_r($message);
-
-if($numbers !='' && $links !='' && $message !=''){
-  } else {
-    echo "Please fill the empty boxes" ;
-  };
-
-/*print_r ($numbers);
-echo '<br>';
-echo '<br>';
-print_r($links);
-*/
-?>
--- vendor/input.php
+++ vendor/input.php
@@ -7,15 +7,116 @@
     <title>Testing for using HTML in same project</title>
   </head>
   <body>
-      <form action="./beforeSending.php" method="post"><br>
-      Numbers: <textarea type="text" name="numbers"> </textarea><br>
-      Links: <textarea type="text" name="links"></textarea> <br>
-      Message: <textarea type="text" name="message"></textarea><br><br>
 
-      <input type="submit" >
+  <?php
+
+//Initializes autoload, Client from Twilio and gets the 2 variables
+
+require_once "../vendor/autoload.php";
+use Twilio\Rest\Client;
+$sid =  getenv("TWILIO_ACCOUNT_SID");
+$token = getenv("TWILIO_AUTH_TOKEN");
+
+//Define variables and set to empty values
+  $numbersErr = '';
+  $linksErr = '';
+  $messageErr = '';
+  
+  
+  // When submit checks if fields are filled, if not prints an error
+
+  if (isset($_POST['submit'])){
+    $ok=true;
+
+      if(!isset($_POST['numbers']) || $_POST['numbers']=='' ){
+      $ok=false;
+      $numbersErr= "Please insert numbers";
+       } else {
+        $numbers1 = array_filter(explode("\r\n", $_POST["numbers"]?? ""));
+        $numbers=preg_replace("/05/","+9725", $numbers1, 1);
+       };
+     
+     if(!isset($_POST['links']) || $_POST['links']==''){
+       $ok=false;
+       $linksErr= "Please insert links";
+     }else{
+      $links=array_filter(explode("\r\n",$_POST["links"]??""));
+     };
+
+     if(!isset($_POST['message']) || $_POST['message']==''){
+       $ok=false;
+       $messageErr="Please write a message to send";
+     }else{
+      $message= $_POST["message"]?? "";
+     }
+
+  
+  
+  
+  
+
+  
+
+
+
+
+//if validation is ok
+
+if($ok){
+
+for($i=0; $i<= count($numbers);$i++){
+
+$twilio = new Client($sid, $token);
+$message = $twilio->messages
+                  ->create("whatsapp:$numbers[$i]", // to
+                           [
+                               "from" => "whatsapp:+14155238886",
+                               "body" => "$message + 
+                                $links[$i]",
+                               
+                              
+                           ]
+                  );
+              
+
+print($message->sid);
+$client->setLogLevel('debug');
+$feedback = $twilio->messages($sid)
+->feedback
+->create();
+
+print($feedback->messageSid);
+}
+
+}
+}
+/*
+print_r($numbers1);
+echo '<br>';
+print_r($links);
+echo '<br>';
+print_r ($message);
+*/
+
+  ?>
+
+
+      <form action="" method="post"><br>
+
+      Numbers: <textarea type="text" name="numbers"></textarea>
+      <span class="error">* <?php echo $numbersErr;?></span>
+  <br><br>
+      Links: <textarea type="text" name="links"></textarea>
+      <span class="error">* <?php echo $linksErr;?></span>
+  <br><br>
+      Message: <textarea type="text" name="message"></textarea>
+      <span class="error">* <?php echo $messageErr;?></span>
+  <br><br>
+      <input type="submit" name="submit">
 
       </form>
 
 
   </body>
 </html>
+
